6d86bfa9ac9006d917ecc8a7d2e74f8760089fcf
wangyijinccx <897168791@qq.com>
2017-09-05 16:03:04 +0800

    返回带参数二维码

--- vending-api/src/main/java/com/ipeaksoft/moneyday/api/controller/VendQRController.java
+++ vending-api/src/main/java/com/ipeaksoft/moneyday/api/controller/VendQRController.java
@@ -2,7 +2,10 @@ package com.ipeaksoft.moneyday.api.controller;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,19 +13,49 @@ import javax.servlet.http.HttpServletResponse;
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ipeaksoft.moneyday.core.service.HttpService;
 
 @Controller
 @RequestMapping(value = "/qr")
 public class VendQRController extends BaseController {
 
+	@Autowired
+	private HttpService httpService;
+
+	@ResponseBody
+	@RequestMapping("getParaQr")
+	public void getParaQr(HttpServletRequest request, Integer type,
+			HttpServletResponse response) {
+		try {
+			URL url = new URL(
+					"https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=gQHi8DoAAAAAAAAAASxodHRwOi8vd2VpeGluLnFxLmNvbS9xL0UweTNxNi1sdlA3RklyRnNKbUFvAAIELdnUUgMEAAAAAA%3D%3D");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(5 * 1000);
+			InputStream inStream = conn.getInputStream();// 通过输入流获取图片数据
+			byte data[] = readInputStream(inStream);
+			// inStream.read(data); //读数据
+			// inStream.close();
+			response.setContentType("image/jpg"); // 设置返回的文件类型
+			OutputStream os = response.getOutputStream();
+			os.write(data);
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	@ResponseBody
 	@RequestMapping("getQr")
-	public void addForHost(HttpServletRequest request, String token,
-			String device, Integer type, HttpServletResponse response) {
-		String qrtext ="http://xiguamei.com";
+	public void addForHost(HttpServletRequest request,
+			HttpServletResponse response) {
+		String qrtext = "http://xiguamei.com";
 
 		ByteArrayOutputStream out = QRCode.from(qrtext).to(ImageType.PNG)
 				.stream();
@@ -39,6 +72,17 @@ public class VendQRController extends BaseController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static byte[] readInputStream(InputStream inStream) throws Exception {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[2048];
+		int len = 0;
+		while ((len = inStream.read(buffer)) != -1) {
+			outStream.write(buffer, 0, len);
+		}
+		inStream.close();
+		return outStream.toByteArray();
 	}
 
 }
