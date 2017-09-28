5954170837efe39bb873ebaa396a5fa4cebb3599
wangyijinccx <897168791@qq.com>
2017-09-28 19:50:35 +0800

    网页授权流程修改

--- vending-api/src/main/java/com/ipeaksoft/moneyday/api/controller/VendOauthContrller.java
+++ vending-api/src/main/java/com/ipeaksoft/moneyday/api/controller/VendOauthContrller.java
@@ -26,8 +26,6 @@ public class VendOauthContrller extends BaseController {
 
 	/**
 	 * 获取code_url
-	 * 1：应该在页面拼接好或者直接调用者里生成url
-	 * 2：暂定是在确认支付的时候调用
 	 * @param goodId
 	 * @param companyId
 	 * @param price
@@ -35,20 +33,14 @@ public class VendOauthContrller extends BaseController {
 	 */
 	@ResponseBody
 	@RequestMapping(value = "oauth")
-	public String oauth(String goodId, String companyId, String price, String num) {
+	public String oauth(String goodId) {
 		// 价格可以从数据库取 也可以页面传，先考虑页面传
 		String url = "";
 		try {
-			JSONObject result = new JSONObject();
 			String redirectUrl = "/oauth/code";
-			result.put("goodId", goodId);
-			result.put("companyId", companyId);
-			result.put("price", price);
-			result.put("num", num);
-			String status = URLEncoder.encode(result.toString(), "UTF-8");
 			redirectUrl = URLEncoder.encode(redirectUrl, "UTF-8");
 			url = wechatWeiXinService.getRequestCodeUrl(redirectUrl, "snsapi_base",
-					status);
+					goodId);
 		} catch (UnsupportedEncodingException e) {
 		}
 		return url;
